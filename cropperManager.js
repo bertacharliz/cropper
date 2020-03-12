85d6f691341ddaa7dc6760b347b19cdf53304376
bertacharliz <azizadk79@yahoo.fr>
2020-03-12 16:04:55 +0100

    Add files via upload

---
+++ cropperManager.js
@@ -0,0 +1,295 @@
+const cropperData = {
+    "crop_zones": [
+        {
+            "selector": {
+                "left": "94.042969%",
+                "top": "77.897727%"
+            },
+            "label": "dog",
+            "crop": {
+                "left": 904,
+                "top": 572,
+                "height": 227,
+                "width": 118
+            },
+            "crop_percent": {
+                "left": "88.281250%",
+                "top": "65.000000%",
+                "height": "25.795455%",
+                "width": "11.523438%"
+            },
+            "confidence": 0.75697505
+        },
+        // {
+        //     "selector": {
+        //         "left": "64.501953%",
+        //         "top": "31.136364%"
+        //     },
+        //     "label": "bowl",
+        //     "crop": {
+        //         "left": 590,
+        //         "top": 234,
+        //         "height": 80,
+        //         "width": 141
+        //     },
+        //     "crop_percent": {
+        //         "left": "57.617188%",
+        //         "top": "26.590909%",
+        //         "height": "9.090909%",
+        //         "width": "13.769531%"
+        //     },
+        //     "confidence": 0.28858382
+        // },
+        {
+            "selector": {
+                "left": "19.091797%",
+                "top": "60.965909%"
+            },
+            "label": "chair",
+            "crop": {
+                "left": 12,
+                "top": 271,
+                "height": 531,
+                "width": 367
+            },
+            "crop_percent": {
+                "left": "1.171875%",
+                "top": "30.795455%",
+                "height": "60.340909%",
+                "width": "35.839844%"
+            },
+            "confidence": 0.78105295
+        },
+        // {
+        //     "selector": {
+        //         "left": "93.212891%",
+        //         "top": "4.488636%"
+        //     },
+        //     "label": "pottedplant",
+        //     "crop": {
+        //         "left": 913,
+        //         "top": 0,
+        //         "height": 79,
+        //         "width": 83
+        //     },
+        //     "crop_percent": {
+        //         "left": "89.160156%",
+        //         "top": "0.000000%",
+        //         "height": "8.977273%",
+        //         "width": "8.105469%"
+        //     },
+        //     "confidence": 0.46592587
+        // },
+        // {
+        //     "selector": {
+        //         "left": "60.839844%",
+        //         "top": "18.465909%"
+        //     },
+        //     "label": "pottedplant",
+        //     "crop": {
+        //         "left": 575,
+        //         "top": 109,
+        //         "height": 107,
+        //         "width": 96
+        //     },
+        //     "crop_percent": {
+        //         "left": "56.152344%",
+        //         "top": "12.386364%",
+        //         "height": "12.159091%",
+        //         "width": "9.375000%"
+        //     },
+        //     "confidence": 0.6380977
+        // },
+        // {
+        //     "selector": {
+        //         "left": "77.978516%",
+        //         "top": "18.977273%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 788,
+        //         "top": 122,
+        //         "height": 90,
+        //         "width": 21
+        //     },
+        //     "crop_percent": {
+        //         "left": "76.953125%",
+        //         "top": "13.863636%",
+        //         "height": "10.227273%",
+        //         "width": "2.050781%"
+        //     },
+        //     "confidence": 0.31488398
+        // },
+        // {
+        //     "selector": {
+        //         "left": "81.054688%",
+        //         "top": "18.977273%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 801,
+        //         "top": 122,
+        //         "height": 90,
+        //         "width": 58
+        //     },
+        //     "crop_percent": {
+        //         "left": "78.222656%",
+        //         "top": "13.863636%",
+        //         "height": "10.227273%",
+        //         "width": "5.664062%"
+        //     },
+        //     "confidence": 0.37132958
+        // },
+        // {
+        //     "selector": {
+        //         "left": "84.619141%",
+        //         "top": "19.602273%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 856,
+        //         "top": 127,
+        //         "height": 91,
+        //         "width": 21
+        //     },
+        //     "crop_percent": {
+        //         "left": "83.593750%",
+        //         "top": "14.431818%",
+        //         "height": "10.340909%",
+        //         "width": "2.050781%"
+        //     },
+        //     "confidence": 0.36063978
+        // },
+        // {
+        //     "selector": {
+        //         "left": "57.128906%",
+        //         "top": "43.409091%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 574,
+        //         "top": 353,
+        //         "height": 58,
+        //         "width": 22
+        //     },
+        //     "crop_percent": {
+        //         "left": "56.054688%",
+        //         "top": "40.113636%",
+        //         "height": "6.590909%",
+        //         "width": "2.148438%"
+        //     },
+        //     "confidence": 0.28179833
+        // },
+        // {
+        //     "selector": {
+        //         "left": "74.707031%",
+        //         "top": "44.715909%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 700,
+        //         "top": 373,
+        //         "height": 41,
+        //         "width": 130
+        //     },
+        //     "crop_percent": {
+        //         "left": "68.359375%",
+        //         "top": "42.386364%",
+        //         "height": "4.659091%",
+        //         "width": "12.695312%"
+        //     },
+        //     "confidence": 0.4497443
+        // },
+        // {
+        //     "selector": {
+        //         "left": "59.765625%",
+        //         "top": "67.159091%"
+        //     },
+        //     "label": "book",
+        //     "crop": {
+        //         "left": 589,
+        //         "top": 546,
+        //         "height": 90,
+        //         "width": 46
+        //     },
+        //     "crop_percent": {
+        //         "left": "57.519531%",
+        //         "top": "62.045455%",
+        //         "height": "10.227273%",
+        //         "width": "4.492188%"
+        //     },
+        //     "confidence": 0.26139715
+        // },
+        {
+            "selector": {
+                "left": "32.910156%",
+                "top": "16.875000%"
+            },
+            "label": "clock",
+            "crop": {
+                "left": 226,
+                "top": 37,
+                "height": 223,
+                "width": 222
+            },
+            "crop_percent": {
+                "left": "22.070312%",
+                "top": "4.204545%",
+                "height": "25.340909%",
+                "width": "21.679688%"
+            },
+            "confidence": 0.7109983
+        }
+    ],
+}
+
+
+    const objectItemsSelector = function () {
+        let selectorButtonList = '';
+        if (cropperData.crop_zones.length >= 1) {
+            cropperData.crop_zones.forEach((cropObject) => {
+            selectorButtonList += `<button class="SearchWithPhotoCrop__objectSelector" style="left: ${cropObject.selector.left}; top: ${cropObject.selector.top}">
+                <div class="SearchWithPhotoCrop__objectSelectorIcon"></div>
+                <div class="SearchWithPhotoCrop__objectCropPreview" data-crop="${cropObject.crop.left};${cropObject.crop.top};${cropObject.crop.width};${cropObject.crop.height}"></div>
+            </button>`;
+        });
+    }
+
+        const refNode = document.querySelector('.ItemCropper__CropContainer');
+        // refNode.after(selectorButtonList);
+
+        refNode.insertAdjacentHTML('afterend', selectorButtonList);
+    }
+
+
+    const getImageDetails = function (imgSize, naturalCropList) {
+
+    const cropperPreviewOutputList = document.querySelectorAll('.SearchWithPhotoCrop__objectCropPreview');
+    for (const index in naturalCropList) {
+        const naturalCrop = naturalCropList[index].crop;
+        const cropperPreviewOutput = cropperPreviewOutputList[index];
+        cropperPreviewOutput.style.width = `${(imgSize.width * naturalCrop.width) / imgSize.naturalWidth}px`;
+        cropperPreviewOutput.style.height = `${(imgSize.height * naturalCrop.height) / imgSize.naturalHeight}px`;
+        cropperPreviewOutput.style.top = '18px';
+        cropperPreviewOutput.style.left = '18px';
+    }
+ }
+
+
+const image = document.getElementById('CroppedImage');
+
+const cropper = new Cropper(image, {
+    aspectRatio: 16 / 9,
+    ready(){
+        const imageData = cropper.getImageData();
+        getImageDetails(imageData, cropperData.crop_zones) ;
+        console.table(cropper.getImageData());
+    },
+    crop(event) {
+        objectItemsSelector();
+    },
+});
+
+
+
+
